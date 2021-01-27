66ec5cea5bd41512d77a1c44dd928dd22d34fffb
bstizzle <whereyouben@gmail.com>
2021-01-27 02:25:11 -0500

    resized region coordinates

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,8 @@ west = Region.create(name: "west", latMin: 31.2, latMax: 41.9, longMin: -126.4, 
 northwest = Region.create(name: "northwest", latMin: 41.9, latMax: 49.0, longMin: -126.4, longMax: -109.9, centerLat: 45.5, centerLong: -115.8)
 south = Region.create(name: "south", latMin: 25.9, latMax: 41.9, longMin: -109.9, longMax: -86.9, centerLat: 34.4, centerLong: -96.2)
 midwest = Region.create(name: "midwest", latMin: 41.9, latMax: 49.0, longMin: -109.9, longMax: -86.9, centerLat: 44.9, centerLong: -93.8)
-southeast = Region.create(name: "southeast", latMin: 24.7, latMax: 41.9, longMin: -86.9, longMax: -76.2, centerLat: 35.1, centerLong: -80.1)
-northeast = Region.create(name:"northeast", latMin: 41.9, latMax: 48.5, longMin: -86.9, longMax: -66.5, centerLat: 43.2, centerLong: -73.0)
+southeast = Region.create(name: "southeast", latMin: 24.7, latMax: 39.6, longMin: -86.9, longMax: -73.9, centerLat: 35.1, centerLong: -80.1)
+northeast = Region.create(name:"northeast", latMin: 39.6, latMax: 48.5, longMin: -86.9, longMax: -66.5, centerLat: 43.2, centerLong: -73.0)
 
 california = State.create(name: "california", region_id: west.id)
 nevada = State.create(name: "nevada", region_id: west.id)
